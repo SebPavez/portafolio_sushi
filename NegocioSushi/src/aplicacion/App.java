a79d004ab80e91b653b97d4aebe8f65aea82a2af
Sebastián Pavez <seb.pavez@alumnos.duoc.cl>
2016-10-03 01:14:04 -0300

    Ventana Principal
    Se agrega ventana principal, queda pendiente:
    - Arranque desde package App
    - Corrección de listar detalle
    - Navegación desde botón "editar" en las vistas
    - Navegación entre ventanas
    
    ESTA ES LA VERSION SUBIDA A ITERACION 1

--- NegocioSushi/src/aplicacion/App.java
+++ NegocioSushi/src/aplicacion/App.java
@@ -1,14 +1,17 @@
 package aplicacion;
 
+import vistas.VentanaPrincipal;
+
 public class App {
     
     public static void main(String[] args){
-        try{
-            Pruebas pruebas = new Pruebas();
-            pruebas.pruebasPedido();
-        } catch (Exception exc){
-            System.out.println("error en ejecución: "+exc.getMessage());
-        }
+        //EJECUCION DE PRUEBAS
+//        try{
+//            Pruebas pruebas = new Pruebas();
+//            pruebas.pruebasPedido();
+//        } catch (Exception exc){
+//            System.out.println("error en ejecución: "+exc.getMessage());
+//        }
         
         
     }
