513e876e4d694577771659dfc4f0aec823b9e4ec
Sebastián Pavez <seb.pavez@alumnos.duoc.cl>
2016-10-25 12:48:37 -0300

    Avance parcial en agregar pedido

---
+++ Sushi_puntonet/Negocio/DetallePedido.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Negocio
+{
+    public class DetallePedido
+    {
+        private int intIdDetalle;
+
+        public int IdDetalle
+        {
+            get { return intIdDetalle; }
+            set { intIdDetalle = value; }
+        }
+
+        private Producto producto;
+
+        public Producto Producto
+        {
+            get { return producto; }
+            set { producto = value; }
+        }
+        
+        
+
+        private int intCantidad;
+
+        public int Cantidad
+        {
+            get { return intCantidad; }
+            set { intCantidad = value; }
+        }
+        
+        
+    }
+}
